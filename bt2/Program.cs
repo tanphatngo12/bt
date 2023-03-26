006cc87964a099e12dc5a5397bd7e5f86f97795b
tanphatngo12 <tanphat2002@125@gmail.com>
2023-03-26 09:04:15 +0700

    edit code

---
+++ bt2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Nhap so nguyen n");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                sum = sum + i;
+                Console.WriteLine(" " + i);
+            }
+            Console.WriteLine("Tong la" + sum);
+            Console.ReadLine();
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey();
+            }
+            while (key.Key != ConsoleKey.Escape);
+        }
+    }
+}
