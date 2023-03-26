cff199d8c936c2a6b5592b8757197f31eb3d8f94
hv <hv@PM309-16>
2023-03-26 08:38:04 +0700

    Add project files.

---
+++ bt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so nguyen n: ");
+            int n = int.Parse(Console.ReadLine());
+            double gt = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                gt = gt *= i;
+            }
+            Console.WriteLine("Tong: " + gt);
+            Console.ReadLine();
+        }
+    }
+}
